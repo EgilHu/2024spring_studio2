ffd62af7971d7fd3f1c07240727a2986d5ab0cb3
EgilHu <qingccch@gmail.com>
2024-04-23 17:57:36 +0800

    6钟攻击方式对应不同输入按键 随即攻击判断输入是否正确

--- Assets/scripts/EnemyAttackSystem.cs
+++ Assets/scripts/EnemyAttackSystem.cs
@@ -8,13 +8,18 @@ public class EnemyAttackSystem : MonoBehaviour
         LeftSideAttack,
         SinglePalmAttack,
         RightSideAttack,
-        // 添加更多攻击方式
+        MiddleAttack,
+        UpSideAttack,
+        DoublePalmAttack,
     }
 
     public AnimatorOverrideController[] animatorOverrides; // 每种攻击对应的Animator Override Controller
     public GameObject leftSideAttackPrefab;
     public GameObject singlePalmAttackPrefab;
     public GameObject rightSideAttackPrefab;
+    public GameObject middleAttackPrefab;
+    public GameObject upSideAttackPrefab;
+    public GameObject doublePalmAttackPrefab;
     // 添加更多攻击方式的预制体
 
     private Animator animator;
@@ -31,27 +36,86 @@ public class EnemyAttackSystem : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(4f);
-            Attack();
-            // Debug.Log("Attack");
+            StopCoroutine("AttackRoutine"); // 停止之前的协程
+            Attack(); // 开始新的攻击
         }
     }
 
+
     void Attack()
     {
-        EnemyAttackType randomAttack = (EnemyAttackType)Random.Range(0, (int)EnemyAttackType.RightSideAttack + 1);
+        EnemyAttackType randomAttack = (EnemyAttackType)Random.Range(0, (int)EnemyAttackType.DoublePalmAttack + 1);
 
         switch (randomAttack)
         {
             case EnemyAttackType.LeftSideAttack:
+                currentAttackType = EnemyAttackType.LeftSideAttack;
                 PlayAnimation(animatorOverrides[0], leftSideAttackPrefab);
                 break;
             case EnemyAttackType.SinglePalmAttack:
+                currentAttackType = EnemyAttackType.SinglePalmAttack;
                 PlayAnimation(animatorOverrides[1], singlePalmAttackPrefab);
                 break;
             case EnemyAttackType.RightSideAttack:
+                currentAttackType = EnemyAttackType.RightSideAttack;
                 PlayAnimation(animatorOverrides[2], rightSideAttackPrefab);
                 break;
-                // 添加更多攻击方式的case
+            case EnemyAttackType.MiddleAttack:
+                currentAttackType = EnemyAttackType.MiddleAttack;
+                PlayAnimation(animatorOverrides[3], middleAttackPrefab);
+                break;
+            case EnemyAttackType.UpSideAttack:
+                currentAttackType = EnemyAttackType.UpSideAttack;
+                PlayAnimation(animatorOverrides[4], upSideAttackPrefab);
+                break;
+            case EnemyAttackType.DoublePalmAttack:
+                currentAttackType = EnemyAttackType.DoublePalmAttack;
+                PlayAnimation(animatorOverrides[5], doublePalmAttackPrefab);
+                break;
+        }
+
+        StartCoroutine(CheckInputDuringAttack());
+    }
+
+    IEnumerator CheckInputDuringAttack()
+    {
+        bool inputReceived = false;
+        KeyCode correctKey = GetCorrectKeyCode(currentAttackType);
+
+        while (!inputReceived && animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1f)
+        {
+            if (Input.GetKeyDown(correctKey))
+            {
+                inputReceived = true;
+                CorrectDefense();
+            }
+            yield return null;
+        }
+
+        if (!inputReceived)
+        {
+            Debug.Log("YOU DIE");
+        }
+    }
+
+    KeyCode GetCorrectKeyCode(EnemyAttackType attackType)
+    {
+        switch (attackType)
+        {
+            case EnemyAttackType.LeftSideAttack:
+                return KeyCode.Q;
+            case EnemyAttackType.SinglePalmAttack:
+                return KeyCode.Space;
+            case EnemyAttackType.RightSideAttack:
+                return KeyCode.P;
+            case EnemyAttackType.MiddleAttack:
+                return KeyCode.O;
+            case EnemyAttackType.UpSideAttack:
+                return KeyCode.I;
+            case EnemyAttackType.DoublePalmAttack:
+                return KeyCode.K;
+            default:
+                return KeyCode.None;
         }
     }
 
@@ -62,6 +126,11 @@ public class EnemyAttackSystem : MonoBehaviour
         GameObject attackObject = Instantiate(prefab, transform.position, Quaternion.identity);
         AttackAnimationEvent attackEvent = attackObject.AddComponent<AttackAnimationEvent>();
         attackEvent.Initialize(this);
+    }
+
+    public void CorrectDefense()
+    {
+        Debug.Log("Correct Defense!");
     }
 
     public void DestroyPrefab(GameObject prefab)
--- Assets/scripts/PlayerReaction.cs
+++ Assets/scripts/PlayerReaction.cs
@@ -6,11 +6,9 @@ public class PlayerReaction : MonoBehaviour
 
     void Start()
     {
-        // 获取敌人攻击系统的引用
         enemyAttackSystem = FindObjectOfType<EnemyAttackSystem>();
     }
 
-    // 当玩家按下键盘按键时被调用
     void Update()
     {
         // 检查玩家按键并调用相应的反应方法
@@ -18,7 +16,7 @@ public class PlayerReaction : MonoBehaviour
         {
             CheckPlayerReaction(EnemyAttackSystem.EnemyAttackType.LeftSideAttack);
         }
-        else if (Input.GetKeyDown(KeyCode.O))
+        else if (Input.GetKeyDown(KeyCode.Space))
         {
             CheckPlayerReaction(EnemyAttackSystem.EnemyAttackType.SinglePalmAttack);
         }
@@ -26,25 +24,33 @@ public class PlayerReaction : MonoBehaviour
         {
             CheckPlayerReaction(EnemyAttackSystem.EnemyAttackType.RightSideAttack);
         }
+        else if (Input.GetKeyDown(KeyCode.O))
+        {
+            CheckPlayerReaction(EnemyAttackSystem.EnemyAttackType.MiddleAttack);
+        }
+        else if(Input.GetKeyDown(KeyCode.I))
+        {
+            CheckPlayerReaction(EnemyAttackSystem.EnemyAttackType.UpSideAttack);
+        }
+        else if (Input.GetKeyDown(KeyCode.K))
+        {
+            CheckPlayerReaction(EnemyAttackSystem.EnemyAttackType.DoublePalmAttack);
+        }
         // 添加更多按键和对应的反应
     }
 
-    // 检查玩家反应是否正确
     void CheckPlayerReaction(EnemyAttackSystem.EnemyAttackType expectedAttackType)
     {
-        // 获取当前敌人的攻击类型
         EnemyAttackSystem.EnemyAttackType actualAttackType = enemyAttackSystem.GetCurrentAttackType();
 
-        // 判断玩家反应是否正确
         if (actualAttackType == expectedAttackType)
         {
-            // 玩家反应正确，继续游戏
-            Debug.Log("Continue");
+            enemyAttackSystem.CorrectDefense();
         }
         else
         {
-            // 玩家反应错误，游戏结束
             Debug.Log("YOU DIE");
         }
     }
+
 }
