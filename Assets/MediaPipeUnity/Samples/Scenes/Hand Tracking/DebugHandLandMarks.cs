2469ec8789d477ce01427bb432cfafd60f1b6681
yanghd <yanghd@shanghaitech.edu.cn>
2024-04-25 01:23:17 +0800

    [手势输入] 1. 加入手势相应 2. 目前支持双拳和单掌Attack TODO: 双掌Attack和拂掌

--- Assets/MediaPipeUnity/Samples/Scenes/Hand Tracking/DebugHandLandMarks.cs
+++ Assets/MediaPipeUnity/Samples/Scenes/Hand Tracking/DebugHandLandMarks.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Google.Protobuf.Collections;
@@ -16,7 +17,9 @@ public class DebugHandLandMarks : MonoBehaviour
     }
     
     private float previous_bone_length = 0.0f;
-
+    private float previous_bone_length1 = 0.0f;
+    private float previous_bone_length2 = 0.0f;
+    
     public TextMeshProUGUI _debuginfo;
     public TextMeshProUGUI _stiketimeinfo;
     
@@ -29,14 +32,19 @@ public class DebugHandLandMarks : MonoBehaviour
     
     private bool fist_detected = false;
 
+    private bool double_fist_detected = false;
+    
     private bool palm_detected = false;
 
     private PalmState _palmState = PalmState.None;
     
-    private bool forwarding_detected = true;
-
-    private Queue<float> hand_speed_queue = new Queue<float>();
-
+    private bool forwarding_detected = false;
+    
+    private bool double_forwarding_detected = false;
+    
+    private Queue<float> single_hand_speed_queue = new Queue<float>();
+    private Queue<float> left_hand_speed_queue = new Queue<float>();
+    private Queue<float> right_hand_speed_queue = new Queue<float>();
     private int strike_times = 0;
 
     private bool enqueue_stopped = false;
@@ -127,10 +135,8 @@ public class DebugHandLandMarks : MonoBehaviour
                Vector3.Distance(dip_pos, tip_pos);
     }
 
-    bool DetectFist(IReadOnlyList<LandmarkList> _landmarkList)
+    bool DetectFist(RepeatedField<Landmark> landmark)
     {
-        RepeatedField<Landmark> landmark = _landmarkList[0].Landmark;
-        // bool detect_thumb = false;
         bool detect_index = false;
         bool detect_middle = false;
         bool detect_ring = false;
@@ -174,10 +180,27 @@ public class DebugHandLandMarks : MonoBehaviour
 
         return detect_index && detect_middle && detect_ring && detect_pinky;
     }
-    
-    bool DetectPalm(IReadOnlyList<LandmarkList> _landmarkList)
+
+    bool DetectDoubleFist(IReadOnlyList<LandmarkList> _landmarkList)
+    {
+        if (_landmarkList.Count < 2)
+        {
+            return false;
+        }
+        else
+        {
+            return DetectFist(_landmarkList[0].Landmark) && DetectFist(_landmarkList[1].Landmark);
+        }
+    }
+    bool DetectFist(IReadOnlyList<LandmarkList> _landmarkList)
     {
         RepeatedField<Landmark> landmark = _landmarkList[0].Landmark;
+        // bool detect_thumb = false;
+        return DetectFist(landmark);
+    }
+
+    bool DetectPalm(RepeatedField<Landmark> landmark)
+    {
         bool detect_thumb = false;
         bool detect_index = false;
         bool detect_middle = false;
@@ -219,6 +242,20 @@ public class DebugHandLandMarks : MonoBehaviour
         }
 
         return detect_index && detect_middle && detect_ring && detect_pinky;
+    }
+    
+    bool DetectPalm(IReadOnlyList<LandmarkList> _landmarkList)
+    {
+        if (_landmarkList.Count < 2)
+        {
+            return DetectPalm(_landmarkList[0].Landmark);
+        }
+        else
+        {
+            return DetectPalm(_landmarkList[0].Landmark) || DetectPalm(_landmarkList[1].Landmark);
+        }
+
+        
     }
 
     PalmState DetectPalmState(IReadOnlyList<LandmarkList> _landmarkList)
@@ -266,35 +303,76 @@ public class DebugHandLandMarks : MonoBehaviour
                 palm_detected = false;
                 _palmState = PalmState.None;
             }
+
+            if (DetectDoubleFist(_landmarkList))
+            {
+                double_fist_detected = true;
+            }
+            else
+            {
+                double_fist_detected = false;
+            }
         }
 
     }
     
-    float CalculateHandAveragevelocity()
+    float CalculateHandAveragevelocity(Queue<float> queue)
     {
         float sum = 0.0f;
-        foreach (float speed in hand_speed_queue)
+        foreach (float speed in queue)
         {
             sum += speed;
         }
         // Debug.Log(sum);
-        return sum / hand_speed_queue.Count;
+        return sum / queue.Count;
     }
-    
+
+
     public void DebugBonelength(IReadOnlyList<NormalizedLandmarkList> _landmarkList)
     {
         if (_landmarkList != null && !enqueue_stopped)
         {
-            Vector3 index_mcp_pos = new Vector3(_landmarkList[0].Landmark[5].X, _landmarkList[0].Landmark[5].Y, _landmarkList[0].Landmark[5].Z);
-            Vector3 wrist_pos = new Vector3(_landmarkList[0].Landmark[0].X, _landmarkList[0].Landmark[0].Y, _landmarkList[0].Landmark[0].Z);
-            float bone_length = Vector3.Distance(index_mcp_pos, wrist_pos);
-            float bone_length_velocity = bone_length - previous_bone_length;
-            hand_speed_queue.Enqueue(bone_length_velocity);
-            previous_bone_length = bone_length;
-
-            if (hand_speed_queue.Count > num_frames_to_avarage)
+            // single_hand_speed
+            float bone_length_velocity = 0.0f;
+            if (_landmarkList.Count < 2)
             {
-                if (CalculateHandAveragevelocity() > velocity_threshold)
+                Vector3 index_mcp_pos = new Vector3(_landmarkList[0].Landmark[5].X, _landmarkList[0].Landmark[5].Y,
+                    _landmarkList[0].Landmark[5].Z);
+                Vector3 wrist_pos = new Vector3(_landmarkList[0].Landmark[0].X, _landmarkList[0].Landmark[0].Y,
+                    _landmarkList[0].Landmark[0].Z);
+                float bone_length = Vector3.Distance(index_mcp_pos, wrist_pos);
+                bone_length_velocity = bone_length - previous_bone_length;
+                previous_bone_length = bone_length;
+            }
+            else
+            {
+                Vector3 index_mcp_pos = new Vector3(_landmarkList[0].Landmark[5].X, _landmarkList[0].Landmark[5].Y,
+                    _landmarkList[0].Landmark[5].Z);
+                Vector3 wrist_pos = new Vector3(_landmarkList[0].Landmark[0].X, _landmarkList[0].Landmark[0].Y,
+                    _landmarkList[0].Landmark[0].Z);
+                float bone_length = Vector3.Distance(index_mcp_pos, wrist_pos);
+                float left_bone_length_velocity = bone_length - previous_bone_length1;
+
+                Vector3 index_mcp_pos2 = new Vector3(_landmarkList[1].Landmark[5].X, _landmarkList[1].Landmark[5].Y,
+                    _landmarkList[1].Landmark[5].Z);
+                Vector3 wrist_pos2 = new Vector3(_landmarkList[1].Landmark[0].X, _landmarkList[1].Landmark[0].Y,
+                    _landmarkList[1].Landmark[0].Z);
+                float bone_length2 = Vector3.Distance(index_mcp_pos2, wrist_pos2);
+                float right_bone_length_velocity = bone_length2 - previous_bone_length2;
+
+                bone_length_velocity = MathF.Max(left_bone_length_velocity, right_bone_length_velocity);
+                previous_bone_length1 = bone_length;
+                previous_bone_length2 = bone_length2;
+                left_hand_speed_queue.Enqueue(left_bone_length_velocity);
+                right_hand_speed_queue.Enqueue(right_bone_length_velocity);
+            }
+
+            single_hand_speed_queue.Enqueue(bone_length_velocity);
+
+
+            if (single_hand_speed_queue.Count > num_frames_to_avarage)
+            {
+                if (CalculateHandAveragevelocity(single_hand_speed_queue) > velocity_threshold)
                 {
                     forwarding_detected = true;
                     strike_times++;
@@ -305,12 +383,31 @@ public class DebugHandLandMarks : MonoBehaviour
                     forwarding_detected = false;
                 }
 
-                hand_speed_queue.Clear();
+                single_hand_speed_queue.Clear();
             }
 
+            // double_hand_speed
+            if (left_hand_speed_queue.Count > num_frames_to_avarage &&
+                right_hand_speed_queue.Count > num_frames_to_avarage)
+            {
+                if (CalculateHandAveragevelocity(left_hand_speed_queue) > velocity_threshold &&
+                    CalculateHandAveragevelocity(right_hand_speed_queue) > velocity_threshold)
+                {
+                    double_forwarding_detected = true;
+                    strike_times++;
+                    enqueue_stopped = true;
+                }
+                else
+                {
+                    double_forwarding_detected = false;
+                }
+
+                left_hand_speed_queue.Clear();
+                right_hand_speed_queue.Clear();
+            }
         }
     }
-    
+
     public bool DetectFistAttack()
     {
         return fist_detected && forwarding_detected;
@@ -319,5 +416,25 @@ public class DebugHandLandMarks : MonoBehaviour
     public bool DetectFist()
     {
         return fist_detected;
+    }
+
+    public bool DetectDoubleFist()
+    {
+        return double_fist_detected;
+    }
+    
+    public bool DetectPalmAttack()
+    {
+        return palm_detected && forwarding_detected;
+    }
+    
+    public bool DetectDoublePalmAttack()
+    {
+        return palm_detected && double_forwarding_detected;
+    }
+
+    public bool DetectPalmDownAttack()
+    {
+        return palm_detected && (_palmState == PalmState.Down) && forwarding_detected;
     }
 }
--- Assets/scripts/PlayerReaction.cs
+++ Assets/scripts/PlayerReaction.cs
@@ -1,42 +1,64 @@
+using System;
 using UnityEngine;
 
 public class PlayerReaction : MonoBehaviour
 {
     private EnemyAttackSystem enemyAttackSystem;
-
+    private DebugHandLandMarks _debugHandLandMarks;
     void Start()
     {
         enemyAttackSystem = FindObjectOfType<EnemyAttackSystem>();
+        _debugHandLandMarks = FindObjectOfType<DebugHandLandMarks>();
     }
+
+    // void Update()
+    // {
+    //     // 检查玩家按键并调用相应的反应方法
+    //     if (Input.GetKeyDown(KeyCode.Q))
+    //     {
+    //         CheckPlayerReaction(EnemyAttackSystem.EnemyAttackType.LeftSideAttack);
+    //     }
+    //     else if (Input.GetKeyDown(KeyCode.Space))
+    //     {
+    //         CheckPlayerReaction(EnemyAttackSystem.EnemyAttackType.SinglePalmAttack);
+    //     }
+    //     else if (Input.GetKeyDown(KeyCode.P))
+    //     {
+    //         CheckPlayerReaction(EnemyAttackSystem.EnemyAttackType.RightSideAttack);
+    //     }
+    //     else if (Input.GetKeyDown(KeyCode.O))
+    //     {
+    //         CheckPlayerReaction(EnemyAttackSystem.EnemyAttackType.MiddleAttack);
+    //     }
+    //     else if(Input.GetKeyDown(KeyCode.I))
+    //     {
+    //         CheckPlayerReaction(EnemyAttackSystem.EnemyAttackType.UpSideAttack);
+    //     }
+    //     else if (Input.GetKeyDown(KeyCode.K))
+    //     {
+    //         CheckPlayerReaction(EnemyAttackSystem.EnemyAttackType.DoublePalmAttack);
+    //     }
+    //     // 添加更多按键和对应的反应
+    // }
 
     void Update()
     {
-        // 检查玩家按键并调用相应的反应方法
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            CheckPlayerReaction(EnemyAttackSystem.EnemyAttackType.LeftSideAttack);
-        }
-        else if (Input.GetKeyDown(KeyCode.Space))
-        {
-            CheckPlayerReaction(EnemyAttackSystem.EnemyAttackType.SinglePalmAttack);
-        }
-        else if (Input.GetKeyDown(KeyCode.P))
-        {
-            CheckPlayerReaction(EnemyAttackSystem.EnemyAttackType.RightSideAttack);
-        }
-        else if (Input.GetKeyDown(KeyCode.O))
+        if (_debugHandLandMarks.DetectDoubleFist())
         {
             CheckPlayerReaction(EnemyAttackSystem.EnemyAttackType.MiddleAttack);
         }
-        else if(Input.GetKeyDown(KeyCode.I))
+        else if (_debugHandLandMarks.DetectPalmAttack())
         {
-            CheckPlayerReaction(EnemyAttackSystem.EnemyAttackType.UpSideAttack);
+            CheckPlayerReaction(EnemyAttackSystem.EnemyAttackType.SinglePalmAttack);
         }
-        else if (Input.GetKeyDown(KeyCode.K))
+        else if (_debugHandLandMarks.DetectDoublePalmAttack())
         {
             CheckPlayerReaction(EnemyAttackSystem.EnemyAttackType.DoublePalmAttack);
         }
-        // 添加更多按键和对应的反应
+        else if (_debugHandLandMarks.DetectPalmDownAttack())
+        {
+            CheckPlayerReaction(EnemyAttackSystem.EnemyAttackType.UpSideAttack);
+        }
     }
 
     void CheckPlayerReaction(EnemyAttackSystem.EnemyAttackType expectedAttackType)
